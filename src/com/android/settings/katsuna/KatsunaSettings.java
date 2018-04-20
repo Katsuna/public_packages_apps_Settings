3f7c17cb7530cdb98b6c3db1b37fac0fcfaca15d
alkis <a.kafkis@gmail.com>
2018-04-20 20:03:19 +0300

    Katsuna Settings initial activity

---
+++ src/com/android/settings/katsuna/KatsunaSettings.java
@@ -0,0 +1,15 @@
+package com.android.settings.katsuna;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.settings.R;
+
+public class KatsunaSettings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.katsuna_settings);
+    }
+}
